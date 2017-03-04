eb341af4325a260a0dbb9856b208e1d84f8b7887
David Gonzales <David Gonzales>
2017-03-04 09:21:15 -0700

    Cleaned out old and added static wsite

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+/* pull in mongoose and connect to mlab mongo database*/
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connect(process.env.mongodb_books);
+
+/* Create our express routing app */
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+/* Add Generic Middleware */
+/* set port */
+app.set('port', (process.env.PORT || 3000));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+/*
+ * https://expressjs.com/en/starter/static-files.html
+ * Serve static files from this directory
+ * app.use = middleware
+ */
+app.use(express.static('build'));
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+
+/* parses body of incoming json */
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+/* API handler sends message and displays on screen */
+app.get('/', function (req, res) {
+    res.send('Welcome to my API, said Dave');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+/* Pull in book model and genericRoute and pass models to genericRoute*/
+var Book = require('./models/bookModel');
+var bookRouter = require('./Routes/genericRoute')(Book);
+
+/* Apply middleware bookRoute to app with the route /api/books */
+app.use('/api/books', bookRouter);
+//app.use('/api/authors', authorRouter);
+
+
+/* have app listen on port */
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
