9a477feae54e9da03d0e270fedef5caaaf0b7258
David Gonzales <David Gonzales>
2017-03-22 22:30:07 -0600

    fixed express/mongoose to work with mongo.

--- Routes/genericRoute.js
+++ Routes/genericRoute.js
@@ -2,98 +2,22 @@
  * Created by David on 10/12/2016.
  */
 var express = require('express');
-var parseUrl = require('parseurl');
+
 var routes = function (MongooseAPIModel, apiPath) {
     var genericRouter = express.Router();
 
-    genericRouter.route('/')
+    genericRouter.route('/:page/:section/:subsetion/:cardNum')
         .post(function (req, res) {
             var mongooseAPIObj = new MongooseAPIModel(req.body);
             mongooseAPIObj.save();
             res.status(201).send(mongooseAPIObj);
         })
         .get(function (req, res) {
-
-
-            var query = {};
-            if (req.query.genre) {
-                query.genre = req.query.genre;
-            }
-            MongooseAPIModel.find(query, function (err, mongooseAPIObjs) {
+            MongooseAPIModel.find({url: req.path}, function (err, cards) {
                 if (err) {
-                    res.status(500).send(err);
+                    return res.status(500).send(err);
                 } else {
-                    var returnJSONArray = [];
-                    mongooseAPIObjs.forEach(function (element, index, array) {
-                        var newJSON = element.toJSON();
-                        newJSON.links = {};
-                        newJSON.links.self = `http://${req.headers.host}/${req.baseUrl}/${newJSON._id}`;
-                        returnJSONArray.push(newJSON);
-                    });
-                    /* res.json returns array of json */
-                    res.json(returnJSONArray);
-                }
-            });
-        });
-    genericRouter.use('/:page/:section/:subsetion/:card', function (req, res, next) {
-        console.log('Dave', req.baseUrl);
-
-        MongooseAPIModel.findOne({'uri': req.baseUrl}, function (err, mongooseAPIObj) {
-            if (err) {
-                res.status(500).send(err);
-            }
-            else if (mongooseAPIObj) {
-                req.mongooseAPIObj = mongooseAPIObj;
-                next();
-            } else {
-                res.status(404).send('no book found');
-            }
-        });
-
-    });
-    genericRouter.route('/:page/:section/:subsetion/:card')
-        .get(function (req, res) {
-            console.log('Dave1', req.baseUrl);
-            var returnJSON = req.mongooseAPIObj.toJSON();
-            returnJSON.links = {};
-            var newLink = `http://${req.headers.host}/${apiPath}/?genre=${returnJSON.genre}`;
-            returnJSON.links.FilterByThisGenre = newLink.replace(' ', '%20');
-            res.json(returnJSON);
-        })
-        .put(function (req, res) {
-            for (var p in req.body) {
-                req.mongooseAPIObj[p] = req.body[p];
-            }
-            req.mongooseAPIObj.save(function (err) {
-                if (err) {
-                    res.status(500).send(err);
-                } else {
-                    res.json(req.mongooseAPIObj)
-                }
-            });
-
-        })
-        .patch(function (req, res) {
-            if (req.body._id) {
-                delete req.body._id
-            }
-            for (var p in req.body) {
-                req.mongooseAPIObj[p] = req.body[p];
-            }
-            req.mongooseAPIObj.save(function (err) {
-                if (err) {
-                    res.status(500).send(err);
-                } else {
-                    res.json(req.mongooseAPIObj)
-                }
-            });
-        })
-        .delete(function (req, res) {
-            req.mongooseAPIObj.remove(function (err) {
-                if (err) {
-                    res.status(500).send(err);
-                } else {
-                    res.status(204).send('Removed');
+                    return res.json(cards);
                 }
             });
         });
--- index.js
+++ index.js
@@ -1,38 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var cors = require('cors');
 var modelTypes = require('./models/modelTypes');
-
 var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 
-var db = mongoose.connect(process.env.mongodb_books);
+var db = mongoose.connect(process.env.MONGODB_URI);
 
 var app = express();
-
-app.use(cors());
 app.set('port', (process.env.PORT || 3000));
-
 app.use(express.static('build'));
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.send('Welcome to my API, said Dave');
-});
-
-var Book = require('./models/bookModel');
-var bookRouter = require('./Routes/genericRoute')(Book, modelTypes.books);
 var Card = require('./models/cardModel');
 var cardRouter = require('./Routes/genericRoute')(Card, modelTypes.cards);
-
- app.use('/api', bookRouter);
-// app.use('/api/cards', cardRouter);
+app.use('/api', cardRouter);
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
-
-
