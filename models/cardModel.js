f70ec33d7c4e152b999c5e522e5e38fdfd19d086
David Gonzales <David Gonzales>
2017-03-25 02:28:01 -0600

    changed card  model to grb accordian paragraph with video

--- models/cardModel.js
+++ models/cardModel.js
@@ -2,16 +2,38 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var cardModel = new Schema({
-    title: {type: String},
-    question: {type: String},
-    answers: [{type: String}],
-    conclusion: {type: String}
+
+
+
+
+    url : { type: String },
+    type : { type: String },
+    title: { type: String },
+    Vid : {
+        type : { type: String },
+        src : { type: String },
+        border : { type: String },
+        width : { type: String },
+        height : { type: String },
+        frameborder : { type: String },
+        allowfullscreen : { type: String }
+    },
+    topic : { type: String },
+    reasonDetailfFactTransitions: [
+        {
+            rdft: { type: String },
+            explainations: [
+                {
+                    explaination: { type: String },
+                }
+            ]
+        },
+
+    ],
+    conclusion: { type: String }
+
+
 });
-// var cardModel = new Schema({
-//     title: {type: String},
-//     question: {type: String},
-//     answers: [{type: String}],
-//     conclusion: {type: String}
-// });
+
 
 module.exports = mongoose.model('Card', cardModel);
