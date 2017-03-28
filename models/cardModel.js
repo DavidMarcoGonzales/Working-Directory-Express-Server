9d2df5f68ba80d79ec5252ecd450ed5ebfdc7480
David Gonzales <David Gonzales>
2017-03-28 16:27:09 -0600

    removed nested arrays, cuz they suck.

--- models/cardModel.js
+++ models/cardModel.js
@@ -2,37 +2,31 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var cardModel = new Schema({
-
-
-
-
-    url : { type: String },
-    type : { type: String },
+    url: { type: String },
+    type: { type: String },
     title: { type: String },
-    Vid : {
-        type : { type: String },
-        src : { type: String },
-        border : { type: String },
-        width : { type: String },
-        height : { type: String },
-        frameborder : { type: String },
-        allowfullscreen : { type: String }
+    Vid: {
+        type: { type: String },
+        src: { type: String },
+        border: { type: String },
+        width: { type: String },
+        height: { type: String },
+        frameborder: { type: String },
+        allowfullscreen: { type: String }
     },
-    topic : { type: String },
-    rdfts: [
-        {
-            rdft: { type: String },
-            explainations: [
-                {
-                    explaination: { type: String },
-                }
-            ]
-        },
-
-    ],
+    rdft1: { type: String },
+    rdft2: { type: String },
+    rdft3: { type: String },
+    explaination1: { type: String },
+    explaination2: { type: String },
+    explaination3: { type: String },
+    explaination4: { type: String },
+    explaination5: { type: String },
+    explaination6: { type: String },
+    explaination7: { type: String },
+    explaination8: { type: String },
+    explaination9: { type: String },
     conclusion: { type: String }
-
-
 });
 
 
