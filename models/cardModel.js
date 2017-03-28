b43cfc8ef27c9a31f5dd8d8d893c18f58daf6277
David Gonzales <David Gonzales>
2017-03-28 17:52:44 -0600

    one more time model

--- models/cardModel.js
+++ models/cardModel.js
@@ -13,7 +13,7 @@ var cardModel = new Schema({
         frameborder: { type: String },
         allowfullscreen: { type: String }
     },
-    myArray: [{body: { type: String }} ],
+    myArray: [ String  ],
     topic: { type: String },
     accordBody: { type: String },
     conclusion: { type: String }
