48c98c15c61a00ee1a8731dbe40d9558460f6637
David Gonzales <David Gonzales>
2017-03-28 17:03:12 -0600

    fixing object once again

--- models/cardModel.js
+++ models/cardModel.js
@@ -3,7 +3,6 @@ var Schema = mongoose.Schema;
 
 var cardModel = new Schema({
     url: { type: String },
-    type: { type: String },
     title: { type: String },
     Vid: {
         type: { type: String },
@@ -14,20 +13,9 @@ var cardModel = new Schema({
         frameborder: { type: String },
         allowfullscreen: { type: String }
     },
-    rdft1: { type: String },
-    rdft2: { type: String },
-    rdft3: { type: String },
-    explaination1: { type: String },
-    explaination2: { type: String },
-    explaination3: { type: String },
-    explaination4: { type: String },
-    explaination5: { type: String },
-    explaination6: { type: String },
-    explaination7: { type: String },
-    explaination8: { type: String },
-    explaination9: { type: String },
+
+    topic: { type: String },
+    accordBody: { type: String },
     conclusion: { type: String }
 });
-
-
 module.exports = mongoose.model('Card', cardModel);
