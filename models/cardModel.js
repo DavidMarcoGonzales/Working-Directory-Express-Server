b009b6c974fa448a229dc39df8987a59aea1e9e7
David Gonzales <David Gonzales>
2017-03-29 12:50:29 -0600

    changing attributes once again

--- models/cardModel.js
+++ models/cardModel.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var cardModel = new Schema({
-    url: { type: String },
+    currentURN: { type: String },
     title: { type: String },
     vid: {
         type: { type: String },
