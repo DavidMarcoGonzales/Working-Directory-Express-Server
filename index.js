7ee1b645255db0c83ed9ac677f6b9ef3ea6667c7
David Gonzales <David Gonzales>
2017-04-18 22:58:14 -0600

    all good now

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var modelTypes = require('./models/modelTypes');
-var cors = require('cors');
+//var cors = require('cors');
 var mongoose = require('mongoose');
-//var expressJWT = require('express-jwt');
-//var jwt = require('jsonwebtoken');
+var expressJWT = require('express-jwt');
+var jwt = require('jsonwebtoken');
 
 
 var db = mongoose.connect(process.env.MONGODB_URI);
 
 var app = express();
 app.set('port', (process.env.PORT || 3000));
-app.use(cors());
+//app.use(cors());
 app.use(express.static('build'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 //app.use(expressJWT({secret:'I love domestic longhaired cats the most'}).unless({path:['/login','/api/Workbook','/api/Situational_Awareness']}))
-
+console.log("using 3000");
 
 // app.post('/login', function(req,res){
 //     if(!req.body.username){
