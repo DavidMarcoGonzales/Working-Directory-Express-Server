ae37fba3f6e11771ed5dfca197a2bc8dc8db9621
David M. Gonzales <david_m_gonzales@hotmail.com>
2017-12-23 20:37:32 -0700

    trying to make magic

---
+++ Routes/genericRouteold.js
@@ -0,0 +1,20 @@
+/**
+ * Created by David on 10/12/2016.
+ */
+var express = require('express');
+
+var routes = function (MongooseAPIModel) {
+    var genericRouter = express.Router();
+    genericRouter.route('/:section/:subsetion/:cardNum')
+        .get(function (req, res) {
+            MongooseAPIModel.find({_id: req.originalUrl}, function (err, cards) {
+                if (err) {
+                    return res.status(500).send(err);
+                } else {
+                    return res.json(cards);
+                }
+            });
+        });
+    return genericRouter;
+};
+module.exports = routes;
