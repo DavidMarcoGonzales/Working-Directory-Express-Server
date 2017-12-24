15989c75f35c8d9b8191452b055a8be257a736d8
David M. Gonzales <david_m_gonzales@hotmail.com>
2017-12-23 21:12:37 -0700

    goback

--- Routes/genericRoute.js
+++ Routes/genericRoute.js
@@ -7,7 +7,7 @@ var routes = function (MongooseAPIModel) {
     var genericRouter = express.Router();
     genericRouter.route('/:section/:subsetion/:cardNum')
         .get(function (req, res) {
-            MongooseAPIModel.find({_id: req.originalUrl}, function (err, cards) {
+            MongooseAPIModel.find({currentURN: req.originalUrl}, function (err, cards) {
                 if (err) {
                     return res.status(500).send(err);
                 } else {
